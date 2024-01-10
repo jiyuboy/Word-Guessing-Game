a42296befee3ca43c43aaf9077318cd2c3796f22
JiyuBoy <88592206+jiyuboy@users.noreply.github.com>
2024-01-10 18:52:50 +0100

    Create word.js

---
+++ JS/word.js
@@ -0,0 +1,175 @@
+const wordList = [
+  {
+    word: "python",
+    hint: "programming language",
+  },
+  {
+    word: "guitar",
+    hint: "a musical instrument",
+  },
+  {
+    word: "aim",
+    hint: "a purpose or intention",
+  },
+  {
+    word: "venus",
+    hint: "planet of our solar system",
+  },
+  {
+    word: "gold",
+    hint: "a yellow precious metal",
+  },
+  {
+    word: "goIang",
+    hint: "programming language",
+  },
+  {
+    word: "coding",
+    hint: "related to programming",
+  },
+  {
+    word: "matrix",
+    hint: "science fiction movie",
+  },
+  {
+    word: "bugs",
+    hint: "related to programming",
+  },
+  {
+    word: "avatar",
+    hint: "epic science fiction film",
+  },
+  {
+    word: "gif",
+    hint: "a file format for image",
+  },
+  {
+    word: "jocing",
+    hint: "humouros or flippant",
+  },
+  {
+    word: "mental",
+    hint: "relating to the mind",
+  },
+  {
+    word: "map",
+    hint: "diagram representation of area",
+  },
+  {
+    word: "island",
+    hint: "land surraounded by water",
+  },
+  {
+    word: "hockey",
+    hint: "a famous outdoor game",
+  },
+  {
+    word: "chess",
+    hint: "game played by two players",
+  },
+  {
+    word: "act",
+    hint: "the doing of a thing",
+  },
+  {
+    word: "github",
+    hint: "code hosting platform",
+  },
+  {
+    word: "png",
+    hint: "type of raster image file",
+  },
+  {
+    word: "silver",
+    hint: "precious greyish-white metal",
+  },
+  {
+    word: "java",
+    hint: "programming language",
+  },
+  {
+    word: "google",
+    hint: "famous search engining",
+  },
+  {
+    word: "panana",
+    hint: "yellow long curved fruit",
+  },
+  {
+    word: "venice",
+    hint: "famous city of waters",
+  },
+  {
+    word: "mysql",
+    hint: "a relational database system",
+  },
+  {
+    word: "napal",
+    hint: "a developing country",
+  },
+  {
+    word: "flute",
+    hint: "a musical instrument",
+  },
+  {
+    word: "crypto",
+    hint: "related to cryptocurrency",
+  },
+  {
+    word: "tesla",
+    hint: "unit of magnetic flux density",
+  },
+  {
+    word: "rocet",
+    hint: "space vehicule or missile",
+  },
+  {
+    word: "mars",
+    hint: "vecond smallest planet",
+  },
+  {
+    word: "apple",
+    hint: "mobile manufacture",
+  },
+  {
+    word: "proxy",
+    hint: "related to exchanging message",
+  },
+  {
+    word: "html",
+    hint: "markup language for the web",
+  },
+  {
+    word: "cloud",
+    hint: "condenced watery vapour",
+  },
+  {
+    word: "brake",
+    hint: "make a moving vehicule slow",
+  },
+
+  {
+    word: "air",
+    hint: "the invesible gaseous",
+  },
+  {
+    word: "idea",
+    hint: "a thought or suggestion",
+  },
+  {
+    word: "server",
+    hint: "related to computer or system",
+  },
+  {
+    word: "svg",
+    hint: "a vector image format",
+  },
+  {
+    word: "jpeg",
+    hint: "a small image file format",
+  },
+  {
+    word: "search",
+    hint: "act to find something",
+  },
+];
